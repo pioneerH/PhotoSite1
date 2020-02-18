c495b81215a32367b76aee11aebcfe032058dcc6
Hailun Chen <hailunchensh@gmail.com>
2020-02-17 21:48:33 -0800

    edit index.html.erb for both photo and user

---
+++ app/controllers/photo_controller.rb
@@ -0,0 +1,9 @@
+class PhotoController < ApplicationController
+  def index
+    @user = User.joins(:photos)
+    @users = User.find(params[:id])
+    @photos = Photo.joins(:comments)
+    @photos = @users.photos
+
+  end
+end
